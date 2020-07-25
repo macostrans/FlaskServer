5e9c391485fc75005c5f846b317dc54f4275300d
macostrans <kannanoferode@gmail.com>
2020-07-25 13:23:06 +0530

    Table of Contents Page

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
+import plotly.graph_objs as go
+import plotly.offline as pyo
 from flask import Flask
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/')
 def HelloWorld():
-    return "My First Flask Hello World2"
+    df = pd.read_excel(r"https://docs.google.com/spreadsheets/d/1NQAgiLylrdEYLjGWXdc8J3Ns3YmNYZLjVldAf8Mm_kE/export?format=xlsx",sheet_name="index")
+    trace = go.Table(columnwidth=[2,8,5,3,8],header=dict(values=df.columns),cells=dict(values=[df[name] for name in df.columns]))
+    data = [trace]
+    layout = go.Layout(title=dict(text="Table of Contents",x=0.5,y=0.9,xanchor="center",yanchor= "top"))
+    fig = go.Figure(data=data, layout=layout)
+    html_plot = pyo.plot(fig, filename='plotted_file.html', auto_open=False)
+    with open('plotted_file.html', 'r') as f:
+        html = f.read()
+        return html
 
 @app.route('/test')
 def ByeWorld():
